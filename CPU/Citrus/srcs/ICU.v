47cefabc50850f53e230b244c7652dedd551ceba
IceTesla <877790251@qq.com>
2017-09-21 20:25:00 +0800

    ICU的完善

--- CPU/Citrus/srcs/ICU.v
+++ CPU/Citrus/srcs/ICU.v
@@ -15,7 +15,7 @@
 // 
 // Revision:
 // Revision 0.01 - File Created
-// Additional Comments:
+// Additional Comments: 目前没有实现PR的两种工作模式：优先级模式和平等模式
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -39,7 +39,23 @@ module ICU(
 	input i_inta;
 	input i_clk;
 	output o_intr;
-	output[7:0] o_vector;
+	output[6:0] o_vector;
+	
+	function[7:0] getVector;
+        input [7:0] in;
+		input [7:0] vector;
+        case(in)
+            8'b00000001:getVector = vector;
+            8'b00000010:getVector = vector + 1;
+            8'b00000100:getVector = vector + 2;
+            8'b00001000:getVector = vector + 3;
+            8'b00010000:getVector = vector + 4;
+            8'b00100000:getVector = vector + 5;
+            8'b01000000:getVector = vector + 6;
+            8'b10000000:getVector = vector + 7;
+            default:getVector = vector;
+        endcase
+    endfunction
 	
 	//各寄存器输入输出
 	wire[7:0] ISR_data_in,ISR_data_out;
@@ -50,11 +66,18 @@ module ICU(
 	
 	assign IRR_data_in = i_interrupt,
 		PR_data_in = ~IMR_data_out & IRR_data_out,
+		PR_data_in[7] = ~PR_data_in[7],
+		PR_data_in[6] = ~PR_data_in[6],
+		PR_data_in[5] = ~PR_data_in[5],
+		PR_data_in[4] = ~PR_data_in[4],
+		PR_data_in[3] = ~PR_data_in[3],
+		PR_data_in[2] = ~PR_data_in[2],
+		PR_data_in[1] = ~PR_data_in[1],
+		PR_data_in[0] = ~PR_data_in[0],
 		ISR_data_in = PR_data_out,
 		IMR_data_in = i_data,
-		status_data_in = i_data,
+		status_data_in = i_data;
 		
-	
 	//ICU_ctrl控制逻辑
 	wire status_we;
 	wire status_reset;
@@ -66,21 +89,50 @@ module ICU(
 	wire IRR_reset;
 	wire intr;
 	
-	assign status_we = i_we && i_addr[1] && ~i_addr[0] && cs,
-		status_reset = ~i_addr[1] && ~i_addr[0] && cs,
-		IMR_we = i_we && i_addr[1] && i_addr[0] && cs,
-		IMR_reset = ~i_addr[1] && ~i_addr[0] && cs,
-		ISR_we = ~cs && (ISR_data_out[7]||ISR_data_out[6]||ISR_data_out[5]||ISR_data_out[4]||ISR_data_out[3]||ISR_data_out[2]||ISR_data_out[1]||ISR_data_out[0]),
-		ISR_reset = ~i_addr[1] && ~i_addr[0] && cs || ~i_addr[1] && i_addr[0] && i_inta && cs,
-		IRR_we = ~cs && (ISR_data_out[7]||ISR_data_out[6]||ISR_data_out[5]||ISR_data_out[4]||ISR_data_out[3]||ISR_data_out[2]||ISR_data_out[1]||ISR_data_out[0]),
-		IRR_reset = ~i_addr[1] && ~i_addr[0] && cs || ~i_addr[1] && i_addr[0] && i_inta && cs;
- 	
+	assign status_we = i_we && i_addr[1] && ~i_addr[0] && i_cs,
+		status_reset = ~i_addr[1] && ~i_addr[0] && i_cs,
+		IMR_we = i_we && i_addr[1] && i_addr[0] && i_cs,
+		IMR_reset = ~i_addr[1] && ~i_addr[0] && i_cs,
+		ISR_we = ~i_cs && ~ISR_data_out[7]&&~ISR_data_out[6]&&~ISR_data_out[5]&&~ISR_data_out[4]&&~ISR_data_out[3]&&~ISR_data_out[2]&&~ISR_data_out[1]&&~ISR_data_out[0],
+		ISR_reset = ~i_addr[1] && ~i_addr[0] && i_cs || ~i_addr[1] && i_addr[0] && i_inta && i_cs,
+		IRR_we = ~i_cs && ~ISR_data_out[7]&&~ISR_data_out[6]&&~ISR_data_out[5]&&~ISR_data_out[4]&&~ISR_data_out[3]&&~ISR_data_out[2]&&~ISR_data_out[1]&&~ISR_data_out[0],
+		IRR_reset = ~i_addr[1] && ~i_addr[0] && i_cs || ~i_addr[1] && i_addr[0] && i_inta && i_cs,
+		o_intr = ~i_cs && (ISR_data_out[7]||ISR_data_out[6]||ISR_data_out[5]||ISR_data_out[4]||ISR_data_out[3]||ISR_data_out[2]||ISR_data_out[1]||ISR_data_out[0]),
+		o_vector = getVector(ISR_data_out,status_data_out);
+		
+	function[7:0] getPR_out;
+		input[2:0] in;
+		input gs;
+		input eo;
+		case({gs:eo:in})
+			5'b01111:getPR_out = 8'b00000001;
+			5'b01110:getPR_out = 8'b00000010;
+			5'b01101:getPR_out = 8'b00000100;
+			5'b01100:getPR_out = 8'b00001000;
+			5'b01011:getPR_out = 8'b00010000;
+			5'b01010:getPR_out = 8'b00100000;
+			5'b01001:getPR_out = 8'b01000000;
+			5'b01000:getPR_out = 8'b10000000;
+			default:getPR_out = 8'b00000000;
+		endcase
+	endfunction	
+	
+	wire PR_enable_o;
+	wire PR_gs;
+	wire PR_enable_i;
+	wire PR_temp;
+	
+	assign PR_enable = i_cs || ISR_data_out[7] || ISR_data_out[6] || ISR_data_out[5] || ISR_data_out[4] || ISR_data_out[3] || ISR_data_out[2] || ISR_data_out[1] || ISR_data_out[0],
+		PR_data_out = getPR_out(PR_temp,)
+	
+	
+	
 	PriorityEncoder8_3 PR(
 		.in_data_n(PR_data_in),
-		.in_enable_n(),
-		.o_data_n(),
-		.o_gs_n(),
-		.o_enable_n()
+		.in_enable_n(PR_enable_i),
+		.o_data_n(PR_temp),
+		.o_gs_n(PR_gs),
+		.o_enable_n(PR_enable_o)
 	);
 	
 	reg_8bit IRR(
