b9efc24a99e2e2b5dbcbfe26ff58bf5e061a6156
liujiapeng <liujiapeng95@hotmail.com>
2017-09-28 14:02:19 +0800

    更新代码生成程序，添加MIPS测试用代码

---
+++ Compiler/definition.h
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+extern int Totalerrors, funcount;
+extern struct allFunc **ALL;
+
+struct AST {
+	char txt[50];
+    int ntno;
+    int procno;
+    int multiplicity;
+    int contain_expr;
+ 	struct AST *parent;
+	struct AST **child;
+};
+
+struct allFunc {
+	char name[50];
+    int type;
+	struct symbolTable *st;
+	struct AST *t;
+};
+
+struct symbolTable {
+    int count;
+    struct symbolTableItem **item;
+};
+
+struct symbolTableItem {
+    char name[50];
+    int pos; // 1-a, 2-t7, 3-s, 4-sp
+    int offset;
+};
+
+struct AST * makeNode(int num);
+struct symbolTable * makeST();
+struct symbolTableItem * makeSTitem();
+void st_add(char *name, int pos, int offset, int funcno);
+char * lookup(char *name, int funcno);
+void GenerateMIPS();
+
+
+
+
