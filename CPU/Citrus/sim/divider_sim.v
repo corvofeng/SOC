4b8b3afb810750a7a4d57a93b1196eb172a4b435
IceTesla <877790251@qq.com>
2017-10-01 17:10:53 +0800

    实现有无符号除法器的控制，添加wallace树无符号乘法

--- CPU/Citrus/sim/divider_sim.v
+++ CPU/Citrus/sim/divider_sim.v
@@ -27,12 +27,13 @@ module divider_sim(
        );
 reg clock = 0;
 reg resetn = 0;
-wire [31:0] A = 1000000000;
-wire [31:0] B = 999999999;
+wire [31:0] A = 32'h8000_0000;
+wire [31:0] B = 32'h7d5f_8a74;
 reg start = 0;
 wire busy;
 wire [31:0] q;
 wire [31:0] r;
+reg symbol;
 
 
 divider uut(
@@ -42,12 +43,15 @@ divider uut(
             .b(B),
             .start(start),
             .busy(busy),
+            .symbol(symbol),
             .q(q),
             .r(r)
         );
 initial begin
     #50 resetn = 1;
-    #10 start = 1;
+    #10 start = 1;symbol = 0;
+    #50 start = 0;
+    #1000 start = 1; symbol =1;
     #50 start = 0;
 end
 
